9c84c7a91d8236e5c22a3e79be3eb99910e5c460
Don Moses <githinjidon@gmail.com>
2020-01-24 09:47:14 +0300

    Intial commit

---
+++ user.py
@@ -0,0 +1,13 @@
+class  User:
+  '''
+  Class that generates instance of users
+  '''
+  def __init__(self,username,password):
+    '''
+    init_ method that defines properties for our objects
+    '''
+    
+    self.username = username
+    self.password = password
+  
+  
