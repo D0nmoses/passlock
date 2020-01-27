a3a8df8a8bdb24e501932c37afa2eb2420a64f70
Don Moses <githinjidon@gmail.com>
2020-01-27 09:59:44 +0300

    add menu function to display menu on login

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 from user import User
 from credentials import Credentials
+
+print("********** Welcome to Password Locker **********")
+decision = input("Would you like to register or login? \n A: Register \n B: Login \n" )
+
+if(decision == "A" or decision == "a"):
+  register()
+else:
+  login()
--- user.py
+++ user.py
@@ -1,3 +1,6 @@
+import csv
+from credentials import Credentials
+
 class  User:
   '''
   Class that generates instance of users
@@ -9,5 +12,59 @@ class  User:
     
     self.username = username
     self.password = password
+
+  def enter_password(self):
+    '''
+    enter_password method checks if password entered matches confirmation password and return the password
+    '''
+    while True:
+      password = input("Enter password: \n")
+      passwordAgain = input("Confirm password: \n")
+      if password != passwordAgain:
+        print("Password and confirmation do not match")
+      else: return password
+
+  def add_user(filename,username,password):
+    '''
+    add_user method writes the new user details to an external file
+    '''
+    with open(filename,'a') as csv_file:
+      a = csv.writer(csv_file,delimiter = ',')
+      data = [username,password]
+      a.writerows(data)
+    
+  def new_user(username,password):
+    user = User(username,password)
+  
+  def register():
+    '''
+    register method allows user to store registation details
+    '''
+    username = input("Enter your preferred username: \n")
+    password = enter_password()
+    new_user(username,password)
+    add_user('user.csv',username,password)
+    login()
+
+  def login():
+    username = input("Enter your username: \n")
+    password = input("Enter your password: \n")
+    menu()
+  
+  def menu(self):
+    menu_choice = print("What would you like to do today: \n A: View my credentials \n B: Add custom credential \n C: Add credential with auto-generated password \n D: Delete a credential")
+    if(menu_choice == "A" or menu_choice == "a"):
+      Credentials.viewCredentials()
+      menu()
+    elif(menu_choice == "D" or menu_choice == "d"):
+      Credentials.deleteCredential()
+      menu()
+    elif(menu_choice == "B" pr menu_choice == "b"):
+      Credentials.addCustomCredential()
+      menu()
+    elif(menu_choice == "C" or menu_choice == "c"):
+      Credentials.addAutoCredential()
+      menu()
+
   
   
